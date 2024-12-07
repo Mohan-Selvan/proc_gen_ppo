a28e252a88923c0005fe0c7ec773196e2905dace
Mohan Selvan <mohan70330@gmail.com>
2024-12-07 03:20:12 +0000

    trained ppo pol_cus_cnn 2_act hanging_restriction

--- ppo.py
+++ ppo.py
@@ -217,7 +217,7 @@ def train(device):
 
     print("Training : Start")
     # # Train the model
-    model.learn(total_timesteps=50000, progress_bar=True, callback=reward_callback, reset_num_timesteps=True)
+    model.learn(total_timesteps=200000, progress_bar=True, callback=reward_callback, reset_num_timesteps=True)
     print("Training : Complete")
 
     # Save the model
